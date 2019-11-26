cbbe8cb9de873e44e984e21c71423d33b11330ab
Joseph Alexandre <j.alexandremmfreitas@gmail.com>
2019-11-25 22:35:48 -0300

    Alterações

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     $("#endereco").val("");
     $("#bairro").val("");
     $("#cidade").val("");
-    $("#uf").val("");
+    $("#estado").val("");
   }
 
   $("#cep").blur(function () {
@@ -26,13 +26,13 @@ $(document).ready(function () {
         $("#endereco").val("...");
         $("#bairro").val("...");
         $("#cidade").val("...");
-        $("#uf").val("...");
+        $("#estado").val("...");
         $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
           if (!("erro" in dados)) {
             $("#endereco").val(dados.logradouro);
             $("#bairro").val(dados.bairro);
             $("#cidade").val(dados.localidade);
-            $("#uf").val(dados.uf);
+            $("#estado").val(dados.uf);
           }
           else {
             limpa_formulário_cep();
